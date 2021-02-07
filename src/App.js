5d421f5895f423bfd3a249e85478776b8ea7b2f9
jinimong <phoongdeng@gmail.com>
2021-02-07 17:05:51 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import useInput from "./hooks/useInput";
+
+const App = () => {
+  const name = useInput("Mr.");
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/hooks/useInput.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    console.log(event.target);
+  };
+  return { value, onChange };
+};
+
+export default useInput;
